9c692e026639700c73fc08d56745b8c9f92fa061
Paulina Weiher <45978460+theCieniu@users.noreply.github.com>
2020-04-15 20:54:17 +0200

    Update guessnumber.js
    I've written my own code before watching the tutorial - wanted to try it completely myself. Now I've added in comments the solusion based on a tutorial "Guessing Game" from Creative JS Course by DevEd
    https://developedbyed.com/courses/844150/lectures/15338720
    The result is similar though I had different approach, this was a really nice lesson for me :)

--- guessnumber.js
+++ guessnumber.js
@@ -34,7 +34,28 @@ checkNumber.addEventListener("click", (f) => {
       checkNumber.insertAdjacentHTML("afterend", writeMissed)
       f.preventDefault();
       }
-
-
-
 })
+
+/* =============================================================
+The code based on a tutorial from The Creative JS Course by DevEd
+https://developedbyed.com/courses/844150/lectures/15338720
+===============================================================
+
+function guessGame(){
+  let randomNr = Math.floor(Math.random() * 10 + 1);
+  let guess;
+  do {
+    guess = prompt("Guess a number between 1 and 10")
+    console.log(guess, randomNr)
+    if (guess > randomNr) {
+      console.log("Too high")
+    } else if (guess < randomNr) {
+      console.log("Too low")
+    }
+    } while (randomNr != guess);
+alert(`You've win! It's ${randomNr}!`)
+}
+
+guessGame()
+
+*/
