38cb96708e21051f85528c2418364bd5517e164c
Paulina Weiher <45978460+theCieniu@users.noreply.github.com>
2020-06-06 18:46:24 +0200

    local storage
    Practiced some local storage and created micro app that memorises words from user input.

--- javascript-fun.js
+++ javascript-fun.js
@@ -1,6 +1,5 @@
 document.title = "Draft";
 
-
 /* ========= GREETINGS ============= */
 
 let today = new Date();
@@ -14,7 +13,7 @@ switch (hourNow) {
   case 21:
   case 20:
   case 19:
-    greeting = "Good evening!"
+    greeting = "Good evening!";
     break;
   case 18:
   case 17:
@@ -23,7 +22,7 @@ switch (hourNow) {
   case 14:
   case 13:
   case 12:
-    greeting = "Good afternoon!"
+    greeting = "Good afternoon!";
     break;
   case 11:
   case 10:
@@ -32,41 +31,42 @@ switch (hourNow) {
   case 7:
   case 6:
   case 5:
-    greeting = "Good morning!"
+    greeting = "Good morning!";
     break;
   case 4:
   case 3:
   case 2:
   case 1:
   case 0:
-    greeting = "Good night!"
+    greeting = "Good night!";
     break;
   default:
-    greeting = "Hi!"
+    greeting = "Hi!";
 }
 
 let greetElement = document.getElementById("greetings");
-greetElement.textContent = greeting
-
+greetElement.textContent = greeting;
 
 /* ========= JAVA SCRIPT IN 30 MINUTES TUTORIAL ============= */
-var listItems = document.getElementById('list-items').getElementsByTagName('li')
-var theButton = document.getElementById('list-button')
-var theList = document.getElementById('list-items')
+var listItems = document
+  .getElementById("list-items")
+  .getElementsByTagName("li");
+var theButton = document.getElementById("list-button");
+var theList = document.getElementById("list-items");
 var theCounter = 1;
-var theHeadline = document.getElementById('headline')
+var theHeadline = document.getElementById("headline");
 
 for (i = 0; i < listItems.length; i++) {
-  listItems[i].innerHTML = "It's a kind of magic"
+  listItems[i].innerHTML = "It's a kind of magic";
 }
 
-listItems[0].innerHTML = "This is first item"
+listItems[0].innerHTML = "This is first item";
 
-theButton.addEventListener('click', newItem)
+theButton.addEventListener("click", newItem);
 
 function newItem() {
   theList.innerHTML += "<li>Something new " + theCounter + "</li>";
-  theCounter++
+  theCounter++;
 }
 
 theList.addEventListener("click", newHeadline);
@@ -75,44 +75,41 @@ function newHeadline(l) {
   if (l.target.nodeName == "LI") {
     theHeadline.innerHTML = l.target.innerHTML;
     for (i = 0; i < l.target.parentNode.children.length; i++) {
-      l.target.parentNode.children[i].classList.remove('highlight')
-    };
+      l.target.parentNode.children[i].classList.remove("highlight");
+    }
     l.target.classList.add("highlight");
   }
 }
 
-
-
-
 /* ========= 10 DAYS WITH JAVA SCRIPT TUTORIALS ============= */
 /* ========= TO DO APP ====================================== */
-document.getElementById("js-test1").addEventListener("click", () => alert("Thanks for clicking"))
+document
+  .getElementById("js-test1")
+  .addEventListener("click", () => alert("Thanks for clicking"));
 
-let todoForm = document.getElementById("todo-form")
-let todoInput = document.getElementById("todo-input")
-let todoList = document.getElementById("todo-list")
+let todoForm = document.getElementById("todo-form");
+let todoInput = document.getElementById("todo-input");
+let todoList = document.getElementById("todo-list");
 
 todoForm.addEventListener("submit", (e) => {
-  e.preventDefault()
-  createLi(todoInput.value)
-})
+  e.preventDefault();
+  createLi(todoInput.value);
+});
 
 function createLi(x) {
-  let todoTask = `<li>${x} <button class="delete" onclick="deleteTask(this)">Delete</button></li>`
-  todoList.insertAdjacentHTML("beforeend", todoTask)
-  todoInput.value = ""
-  todoInput.focus()
+  let todoTask = `<li>${x} <button class="delete" onclick="deleteTask(this)">Delete</button></li>`;
+  todoList.insertAdjacentHTML("beforeend", todoTask);
+  todoInput.value = "";
+  todoInput.focus();
 }
 
 function deleteTask(doneTask) {
-  doneTask.parentElement.remove()
+  doneTask.parentElement.remove();
 }
-
-
 
 /* ========= RANDOM NUMBER GENERATOR  ============= */
 
-let randomForm = document.getElementById("random")
+let randomForm = document.getElementById("random");
 
 randomForm.addEventListener("submit", (e) => {
   let minimal = document.getElementById("range-min").value;
@@ -123,49 +120,44 @@ randomForm.addEventListener("submit", (e) => {
   let writeRandom = `<p>Your random number from ${mini} to ${maxi} is <strong>${randomResult}</strong>.</p>`;
   randomForm.insertAdjacentHTML("beforeend", writeRandom);
   e.preventDefault();
-})
-
-
-
-
+});
 
 /* ========= CREATIVE JAVASCRIPT COURSE BY DEVED ============= */
 /* ========= GUESS A NUMBER ====================================== */
 let minMaxSubmit = document.getElementById("submitRandom");
 let guessForm = document.getElementById("guessForm");
-let randomNumber = 0
+let randomNumber = 0;
 
 minMaxSubmit.addEventListener("click", (e) => {
-  let minimal
+  let minimal;
   let mini = parseInt(document.getElementById("guess-min").value);
   let maximum;
   let maxi = parseInt(document.getElementById("guess-max").value);
-  console.log(mini)
-  console.log(maxi)
+  console.log(mini);
+  console.log(maxi);
 
   randomNumber = Math.floor(Math.random() * (maxi - mini + 1) + mini);
-  console.log(randomNumber)
+  console.log(randomNumber);
 
-  guessForm.classList.remove("hiddenElement")
+  guessForm.classList.remove("hiddenElement");
 
-  e.preventDefault()
-  return randomNumber
-})
+  e.preventDefault();
+  return randomNumber;
+});
 
-
-let checkNumber = document.getElementById("submitGuessButton")
+let checkNumber = document.getElementById("submitGuessButton");
 checkNumber.addEventListener("click", (f) => {
   let guess = document.getElementById("numberGuessInput").value;
-  console.log(guess)
+  console.log(guess);
   if (randomNumber == guess) {
     console.log(`That's it, it's ${randomNumber}`);
-    alert(`That's it, it's ${randomNumber}`)
+    alert(`That's it, it's ${randomNumber}`);
   } else {
-    let writeMissed = `<br>It's not ${guess}, try again!`
-    checkNumber.insertAdjacentHTML("afterend", writeMissed)
+    let writeMissed = `<br>It's not ${guess}, try again!`;
+    checkNumber.insertAdjacentHTML("afterend", writeMissed);
     f.preventDefault();
   }
-})
+});
 
 /* =============================================================
 The code below is based on a tutorial from The Creative JS Course by DevEd
@@ -196,8 +188,8 @@ guessGame()
 
 let anotherList = document.getElementById("another-list");
 let anotherButton = document.getElementById("another-button");
-let anInput = document.getElementById("an-input")
-let anItemNr = document.getElementById("an-item-number-display")
+let anInput = document.getElementById("an-input");
+let anItemNr = document.getElementById("an-item-number-display");
 let anItemList = anotherList.children;
 anItemNr.textContent = anItemList.length;
 
@@ -208,11 +200,11 @@ anotherButton.addEventListener("click", () => {
   anotherList.appendChild(aNewItem);
   anItemNr.textContent = anItemList.length;
   for (it of anItemList) {
-    it.addEventListener("click", anDeleteItem)
+    it.addEventListener("click", anDeleteItem);
   }
   anInput.value = "";
   anInput.focus();
-})
+});
 
 function anDeleteItem(it) {
   it.stopPropagation();
@@ -221,17 +213,13 @@ function anDeleteItem(it) {
   anItemNr.textContent = anItemList.length;
 }
 
-
-
-
-
 /* ========= CSS GRID AND THINGS I like ============= */
 
 let lastGrid = document.getElementById("grid-h");
 let gridLikes = document.querySelectorAll(".grid-like");
 
 for (item of gridLikes) {
-  item.addEventListener("click", discover)
+  item.addEventListener("click", discover);
 }
 
 function discover(e) {
@@ -241,8 +229,63 @@ function discover(e) {
   lastGrid = i;
 }
 
+/* ========= local storage ============= */
 
+let storageInput = document.getElementById("storage-input");
+let memoriseButton = document.querySelector("#memorise button");
+let remindButton = document.getElementById("remind");
+let englishWords = [];
+englishWords = JSON.parse(localStorage.getItem("enMemory"));
+let wordList = document.getElementById("words-list");
+let clearList = document.getElementById("clear-list");
+let clearMemory = document.getElementById("clear-memory");
 
+memoriseButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (englishWords == null) {
+    englishWords = [];
+  }
+
+  let engWord = storageInput.value;
+  englishWords.push(engWord);
+  storageInput.value = "";
+  storageInput.focus();
+  let wordString = JSON.stringify(englishWords);
+  localStorage.setItem("enMemory", wordString);
+  wordList.innerHTML = "";
+});
+
+remindButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  //here something doesn't work perfectly but I don't know why and how
+  if (englishWords == null || englishWords == []) {
+    alert("Nothing to remind!");
+  } else {
+    englishWords = JSON.parse(localStorage.getItem("enMemory"));
+    englishWords.forEach(function (word) {
+      let listElement = document.createElement("li");
+      let text = document.createTextNode(word);
+      listElement.appendChild(text);
+      wordList.appendChild(listElement);
+    });
+  }
+});
+
+clearMemory.addEventListener("click", () => {
+  event.preventDefault();
+  localStorage.clear();
+  englishWords = [];
+  wordList.innerHTML = "";
+});
+
+clearList.addEventListener("click", () => {
+  event.preventDefault();
+  wordList.innerHTML = "";
+});
+
+/* ========= ============================== ============= */
+/* ========= ============================== ============= */
 /* ========= ============================== ============= */
 
 /* this is the end */
