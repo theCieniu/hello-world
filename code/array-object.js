66fd12f946fa49f73df056b1f1b0e3ed2771cf0d
Paulina Weiher <45978460+theCieniu@users.noreply.github.com>
2020-06-15 22:41:17 +0200

    sort and filter array/object

--- code/array-object.js
+++ code/array-object.js
@@ -2,6 +2,8 @@ function cl(arg) {
   console.log(arg);
 }
 
+// ========== FILTERING ALPINISTS =====================
+
 let alpinists = [
   "Tenzing Norgay Sherpa",
   "Edmund Hillary NZ",
@@ -15,25 +17,90 @@ let alpinists = [
   "Tomasz Mackiewicz PL",
   "Simone Moro IT",
   "Rob Hall NZ",
-  "Wanda Anonimowa",
+  //"Wanda Anonimowa",
 ];
 
-//cl("(array) Alpinists: " + alpinists);
+let parentElementAlpinists = document.querySelector("#alpinists span");
+let filterAlpinistsPolishBtn = document.querySelector(
+  "#alpinists button.polish"
+);
+let filterAlpinistsAllBtn = document.querySelector("#alpinists button.all");
+let polishAlpinists = alpinists.filter((name) => name.includes("PL"));
 
-//======== FIND AND FILTER ===========
+function writingAlpinists(alpinistsArray) {
+  let alpinistsHtml = document.createElement("ul");
+  alpinistsArray.forEach((i) => {
+    let text = document.createTextNode(i);
+    let mountain = document.createElement("li");
+    mountain.appendChild(text);
+    alpinistsHtml.appendChild(mountain);
+  });
+  parentElementAlpinists.appendChild(alpinistsHtml);
+}
+writingAlpinists(alpinists);
 
-let womanAlpinist = alpinists.find((name) => {
-  return name.includes("Wanda");
+filterAlpinistsPolishBtn.addEventListener("click", () => {
+  parentElementAlpinists.innerHTML = "";
+  writingAlpinists(polishAlpinists);
+  filterAlpinistsPolishBtn.classList.add("invisible");
+  filterAlpinistsAllBtn.classList.remove("invisible");
 });
-//cl("(find) Woman alpinist: " + womanAlpinist);
+
+filterAlpinistsAllBtn.addEventListener("click", () => {
+  parentElementAlpinists.innerHTML = "";
+  writingAlpinists(alpinists);
+  filterAlpinistsAllBtn.classList.add("invisible");
+  filterAlpinistsPolishBtn.classList.remove("invisible");
+});
+
+// ========== SOTING SUMMITS =====================
+let summits = [
+  { name: "Lhotse", elevation: 8516 },
+  { name: "Everest", elevation: 8848 },
+  { name: "K2", elevation: 8641 },
+  { name: "Rysy", elevation: 2499 },
+  { name: "Nanga Parbat", elevation: 8126 },
+  { name: "Annapurna", elevation: 8091 },
+];
+
+let parentElementSummits = document.querySelector("#summits span");
+let sortSummitsBtnLow = document.querySelector("#summits button.lowest");
+let sortSummitsBtnHigh = document.querySelector("#summits button.highest");
+
+function createSummits() {
+  let summitsHtml = document.createElement("ol");
+  summits.forEach((i) => {
+    let text = document.createTextNode(i.name + ": " + i.elevation + " m ");
+    let mountain = document.createElement("li");
+    mountain.appendChild(text);
+    summitsHtml.appendChild(mountain);
+  });
+  parentElementSummits.appendChild(summitsHtml);
+}
+
+createSummits();
+
+sortSummitsBtnLow.addEventListener("click", () => {
+  parentElementSummits.innerHTML = "";
+  summits.sort((a, b) => a.elevation - b.elevation);
+  createSummits();
+});
+
+sortSummitsBtnHigh.addEventListener("click", () => {
+  parentElementSummits.innerHTML = "";
+  summits.sort((a, b) => b.elevation - a.elevation);
+  createSummits();
+});
+
+/* ===== JUST SOME NOTES FOR MYSELF =====
 
 let polishAlpinists = alpinists.filter((name) => name.includes("PL"));
 //cl("(filter) Polish alpinists: " + polishAlpinists);
 
 //===== SOME AND EVERY ==============
-/* every returns true or false - are every item fullfiling the condition? 
+every returns true or false - are every item fullfiling the condition? 
 good tool to check if everyting is ok with an array, 
-aren't there any empty elements etc. */
+aren't there any empty elements etc. 
 let greatAlpinists1 = alpinists.every((name) => {
   return name.includes("PL");
 });
@@ -51,38 +118,5 @@ let isVisitorAlpinist =
 //cl(isVisitorAlpinist);
 
 // SORTING
-
 alpinists.sort();
-//cl("(sort) Sorted by alphabet: " + alpinists);
-
-// ========== SOTING SUMMITS =====================
-let summits = [
-  { name: "Lhotse", elevation: 8516 },
-  { name: "Everest", elevation: 8848 },
-  { name: "K2", elevation: 8641 },
-  { name: "Rysy", elevation: 2499 },
-  { name: "Nanga Parbat", elevation: 8126 },
-  { name: "Annapurna", elevation: 8091 },
-];
-let parentElement = document.querySelector("#summits span");
-let sortSummitsBtn = document.querySelector("#summits button");
-
-function createSummits() {
-  let summitsHtml = document.createElement("ol");
-  summits.forEach((i) => {
-    let text = document.createTextNode(i.name + ": " + i.elevation + " m ");
-    let mountain = document.createElement("li");
-    mountain.appendChild(text);
-    summitsHtml.appendChild(mountain);
-  });
-  parentElement.appendChild(summitsHtml);
-}
-
-createSummits();
-
-sortSummitsBtn.addEventListener("click", () => {
-  parentElement.innerHTML = "";
-  summits.sort((a, b) => a.elevation - b.elevation);
-  createSummits();
-  sortSummitsBtn.classList.add("invisible");
-});
+//cl("(sort) Sorted by alphabet: " + alpinists); */
